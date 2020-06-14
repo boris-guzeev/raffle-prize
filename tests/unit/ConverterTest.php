36e81f2d5836968f05d0b46f33c8aecbef4b3cc3
Boris Guzeev <guzeevboris@mail.ru>
2020-06-15 02:00:51 +0300

    add test

---
+++ tests/unit/ConverterTest.php
@@ -0,0 +1,24 @@
+<?php
+use \app\models\Converter;
+
+class ConverterTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testConvertToPoints()
+    {
+
+    }
+}
