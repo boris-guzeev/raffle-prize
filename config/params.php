e667a769cc280d825dc85d2715c3796bacb0b145
Boris Guzeev <guzeevboris@mail.ru>
2020-06-14 17:47:15 +0300

    комменты по правилам приложения

--- config/params.php
+++ config/params.php
@@ -5,11 +5,11 @@ return [
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
     'rules' => [
-        'minPrize' => 1,
-        'maxPrize' => 500,
-        'minPoints' => 1,
-        'maxPoints' => 1000,
-        'ratio' => 1.5,
-        'total' => 3000
+        'minPrize' => 1, // минимальный денежный приз
+        'maxPrize' => 500, //макс. денежный приз
+        'minPoints' => 1, // минимальный приз с баллами
+        'maxPoints' => 1000, // максимальный приз с баллами
+        'ratio' => 1.5, // коэфициент конвертации денег в баллы
+        'total' => 3000 // изначальная сумма для розыгрыша
     ]
 ];
