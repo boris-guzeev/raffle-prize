39842c2bd906c132769a6ad422f7f845c3993196
Boris Guzeev <guzeevboris@mail.ru>
2020-06-15 02:02:04 +0300

    Отправка денежных средств со счёта пользователя

--- commands/AdminController.php
+++ commands/AdminController.php
@@ -2,18 +2,51 @@
 
 namespace app\commands;
 
+use app\models\Converter;
 use yii\console\Controller;
 use yii\console\ExitCode;
+use \Yii;
+use yii\db\Exception;
+use yii\db\Query;
 
-class AdminController
+class AdminController extends Controller
 {
     /**
-     * Здесь должна быть отправка, например, через Курл запрос в стороний сервис банка
+     * Отправка денежных средств со счёта пользователя
+     * @param integer $userId ИД пользователя, чьи платежи нужно отправить в банк
+     * @param integer $quantity Количество платежей, которое нужно отправить
      * @return int Exit code
      */
-    public function actionSend($message)
+    public function actionSend($userId, $quantity)
     {
-        echo $message . "\n";
+        // найдем все денежные неотправленные денежные призы в нужном количестве
+        $unsendedMoney = (new Query())
+            ->select(['id', 'sum'])
+            ->from('money_prizes')
+            ->where(['winner_id' => $userId, 'operation_datetime' => null, 'operation_type' => null])
+            ->limit($quantity)
+            ->all();
+
+        //отправим информацию по каждому денежному подарку в импровизированный банк например через cURL через Апи банка
+        foreach ($unsendedMoney as $item) {
+            //$url = 'https://myBank.com/'; $item['sum']; $curl = curl_init(); и тд..
+            // в случае успешного ответа запишем что данная операция выполнена
+            if (true) {
+                try {
+                    Yii::$app->db->createCommand()->update('money_prizes',
+                        [
+                            'operation_datetime' => date('Y-m-d H:i:s'),
+                            'operation_type' => Converter::SEND_TYPE
+                        ], ['id' => $item['id']]
+                    )->execute();
+                } catch (Exception $e) {
+                    $e->getMessage();
+                }
+
+            } else {
+                // пишем лог ошибок или выводим на экран ошибку банка
+            }
+        }
 
         return ExitCode::OK;
     }
