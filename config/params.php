9b780a06e7b3cab67f846262fc5693a997747ebf
Boris Guzeev <guzeevboris@mail.ru>
2020-06-14 10:18:19 +0300

    задали изначальные настройки приложения

--- config/params.php
+++ config/params.php
@@ -4,4 +4,12 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
+    'rules' => [
+        'minPrize' => 1,
+        'maxPrize' => 500,
+        'minPoints' => 1,
+        'maxPoints' => 1000,
+        'ratio' => 1.5,
+        'total' => 3000
+    ]
 ];
