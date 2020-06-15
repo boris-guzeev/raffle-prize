401ddc47284ce6dcef87972f9c9d316390dffc4f
Boris Guzeev <guzeevboris@mail.ru>
2020-06-15 10:38:23 +0300

    fix

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -102,7 +102,7 @@ class SiteController extends Controller
 
     public function actionConvert()
     {
-        Converter::toPoints();
+        Converter::toPoints(Yii::$app->user->identity->id);
     }
     /**
      * Запуск игры
